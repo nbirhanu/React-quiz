1e4b641059118db7b6ba211829dfb77223131444
nbirhanu <nbirhanu80@gmail.com>
2023-08-24 13:15:26 +0300

    handling next button

---
+++ src/components/NestButton.js
@@ -0,0 +1,12 @@
+function NestButton({ dispatch, answer }) {
+	if (answer === null) return null
+	return (
+		<div>
+			<button className="btn btn-ui" onClick={() => dispatch({ type: 'nextQuestion' })}>
+				Next
+			</button>
+		</div>
+	)
+}
+
+export default NestButton
