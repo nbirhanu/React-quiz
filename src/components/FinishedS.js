142302f5ba1a9e873b9f6b11805d37fadafd638a
nbirhanu <nbirhanu80@gmail.com>
2023-08-25 19:24:21 +0300

    handling finished button

---
+++ src/components/FinishedS.js
@@ -0,0 +1,11 @@
+function FinishedS({ point }) {
+	return (
+		<div className="result">
+			<p>
+				You Scored <strong>{point}</strong> Out Of 280
+			</p>
+		</div>
+	)
+}
+
+export default FinishedS
--- src/components/NestButton.js
+++ src/components/NestButton.js
@@ -1,12 +1,21 @@
-function NestButton({ dispatch, answer }) {
+function NestButton({ dispatch, answer, index, numOfQuestion }) {
 	if (answer === null) return null
-	return (
-		<div>
-			<button className="btn btn-ui" onClick={() => dispatch({ type: 'nextQuestion' })}>
-				Next
-			</button>
-		</div>
-	)
+	if (index < numOfQuestion - 1)
+		return (
+			<div>
+				<button className="btn btn-ui" onClick={() => dispatch({ type: 'nextQuestion' })}>
+					Next
+				</button>
+			</div>
+		)
+	if (index === numOfQuestion - 1)
+		return (
+			<div>
+				<button className="btn btn-ui" onClick={() => dispatch({ type: 'finishedScreen' })}>
+					Finished
+				</button>
+			</div>
+		)
 }
 
 export default NestButton
