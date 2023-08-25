d4a61d8289db2205747ec3ee57232f8421c1b4d3
nbirhanu <nbirhanu80@gmail.com>
2023-08-25 20:26:59 +0300

    apply restart logic

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import Loader from './components/Loader'
 import Error from './components/Error'
 import StartScreen from './components/StartScreen'
 import Questions from './components/Question'
+import NestButton from './components/NestButton'
+import Progress from './components/Progress'
+import FinishedS from './components/FinishedS'
+
 //
 const intialState = {
 	questions: [],
 	//loading, error, ready , active, finished
 	status: 'loading',
 	index: 0,
+	answer: null,
+	point: 0,
 }
 function reducer(state, action) {
 	switch (action.type) {
@@ -31,6 +37,31 @@ function reducer(state, action) {
 				...state,
 				status: 'active',
 			}
+		case 'newAnswer':
+			const question = state.questions.at(state.index)
+			return {
+				...state,
+				answer: action.payload,
+				point:
+					action.payload === question.correctOption ? state.point + question.points : state.point,
+			}
+		case 'nextQuestion':
+			return {
+				...state,
+				index: state.index + 1,
+				answer: null,
+			}
+		case 'finishedScreen':
+			return {
+				...state,
+				status: 'finished',
+			}
+		case 'restart':
+			return {
+				...intialState,
+				questions: state.questions,
+				status: 'ready',
+			}
 		default:
 			throw new Error('action unknown')
 	}
@@ -38,9 +69,11 @@ function reducer(state, action) {
 //
 
 function App() {
-	const [{ questions, status, index }, dispatch] = useReducer(reducer, intialState)
+	const [state, dispatch] = useReducer(reducer, intialState)
+	const { questions, status, index, answer, point } = state
 
 	const numOfQuestion = questions.length
+	const maxpossiblePoint = questions.reduce((pre, cur) => pre + cur.points, 0)
 	useEffect(function () {
 		fetch('http://localhost:9000/questions')
 			.then(res => res.json())
@@ -54,7 +87,24 @@ function App() {
 				{status === 'loading' && <Loader />}
 				{status === 'error' && <Error />}
 				{status === 'ready' && <StartScreen numOfQuestion={numOfQuestion} dispatch={dispatch} />}
-				{status === 'active' && <Questions question={questions[index]} />}
+				{status === 'active' && (
+					<>
+						<Progress
+							numOfQuestion={numOfQuestion}
+							index={index}
+							point={point}
+							maxpossiblePoint={maxpossiblePoint}
+						/>
+						<Questions question={questions[index]} dispatch={dispatch} answer={answer} />
+						<NestButton
+							dispatch={dispatch}
+							answer={answer}
+							index={index}
+							numOfQuestion={numOfQuestion}
+						/>
+					</>
+				)}
+				{status === 'finished' && <FinishedS dispatch={dispatch} point={point} />}
 			</Main>
 		</div>
 	)
--- src/components/FinishedS.js
+++ src/components/FinishedS.js
@@ -1,10 +1,15 @@
-function FinishedS({ point }) {
+function FinishedS({ point, dispatch }) {
 	return (
-		<div className="result">
-			<p>
-				You Scored <strong>{point}</strong> Out Of 280
-			</p>
-		</div>
+		<>
+			<div className="result">
+				<p>
+					You Scored <strong>{point}</strong> Out Of 280
+				</p>
+			</div>
+			<button className="btn btn-ui" onClick={() => dispatch({ type: 'restart' })}>
+				Restart
+			</button>
+		</>
 	)
 }
 
